6678baacce9ec809ae7bccb864c21428726570c1
madyanes <madyanes.dev@gmail.com>
2024-09-09 14:55:54 +0700

    Fix: Header component should use a header element
    Not an aside.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const StyledHeader = styled.aside`
+const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
