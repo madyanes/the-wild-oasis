c34121cf31cfe0d243024e3b9a2519e36cd85f94
madyanes <madyanes.dev@gmail.com>
2024-09-16 16:26:20 +0700

    Duplicating Cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,11 +2,13 @@
 
 import { useState } from 'react'
 import styled from 'styled-components'
+import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2'
 
 import { formatCurrency } from '../../utils/helpers'
 
 import CreateCabinForm from './CreateCabinForm'
 import { useDeleteCabin } from './useDeleteCabin'
+import { useCreateCabin } from './useCreateCabin'
 
 const TableRow = styled.div`
   display: grid;
@@ -50,6 +52,8 @@ const Discount = styled.div`
 function CabinRow({ cabin }) {
   const [showForm, setShowForm] = useState(false)
   const { isDeleting, deleteCabin } = useDeleteCabin()
+  const { isCreating, createCabin } = useCreateCabin()
+
   const {
     id: cabinId,
     name,
@@ -58,6 +62,16 @@ function CabinRow({ cabin }) {
     discount,
     image,
   } = cabin
+
+  function handleDuplicate() {
+    createCabin({
+      name: `Copy of ${name}`,
+      maxCapacity,
+      regularPrice,
+      discount,
+      image,
+    })
+  }
 
   return (
     <>
@@ -72,9 +86,14 @@ function CabinRow({ cabin }) {
           <span>&mdash;</span>
         )}
         <div>
-          <button onClick={() => setShowForm((show) => !show)}>Edit</button>
+          <button onClick={handleDuplicate} disabled={isCreating}>
+            <HiSquare2Stack />
+          </button>
+          <button onClick={() => setShowForm((show) => !show)}>
+            <HiPencil />
+          </button>
           <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-            Delete
+            <HiTrash />
           </button>
         </div>
       </TableRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -38,6 +38,8 @@ export async function createEditCabin(newCabin, id) {
   }
 
   // Upload image
+  if (hasImagePath) return cabin // IF the cabin already has image url, no upload
+
   const { error: storageError } = await supabase.storage
     .from('cabin-images')
     .upload(imageName, newCabin.image)
