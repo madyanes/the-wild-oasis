6fa32996087855bd1a82b0540bd2a513d005eec6
madyanes <madyanes.dev@gmail.com>
2024-09-30 01:41:07 +0700

    Put edit cabin form inside modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,18 +1,19 @@
 import Button from '../../ui/Button'
 import Modal from '../../ui/Modal'
-import CabinTable from './CabinTable'
 import CreateCabinForm from './CreateCabinForm'
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens='cabin-form'>
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name='cabin-form'>
-        <CreateCabinForm />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens='cabin-form'>
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name='cabin-form'>
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   )
 }
 
