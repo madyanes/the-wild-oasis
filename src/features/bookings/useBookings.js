623717ca6aeb45bbad37be7eb7427453800b2173
madyanes <madyanes.dev@gmail.com>
2024-10-02 20:48:06 +0700

    API-Side Filtering: Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,33 @@
+import { useSearchParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 
 import { getBookings } from '../../services/apiBookings'
 
 export function useBookings() {
+  const [searchParams] = useSearchParams()
+
+  // FILTER
+  const filterValue = searchParams.get('status')
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : {
+          field: 'status',
+          value: filterValue,
+        }
+  // {
+  //   field: 'totalPrice',
+  //   value: 5000,
+  //   method: 'lte',
+  // }
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   })
 
   return { isLoading, bookings, error }
