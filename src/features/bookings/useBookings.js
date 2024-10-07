5f03fc10ef022e61c1f30913307d8fdbf74a2650
madyanes <madyanes.dev@gmail.com>
2024-10-07 20:00:02 +0700

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -21,13 +21,18 @@ export function useBookings() {
   //   method: 'lte',
   // }
 
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc'
+  const [field, direction] = sortByRaw.split('-')
+  const sortBy = { field, direction }
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   })
 
   return { isLoading, bookings, error }
