5a92930cfdd07011be51022aa17e97aad4594382
madyanes <madyanes.dev@gmail.com>
2024-10-10 14:55:50 +0700

    API-Side Pagination: Paginating Bookings pt.2

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,7 +1,8 @@
 import { getToday } from '../utils/helpers'
 import supabase from './supabase'
+import { PAGE_SIZE } from '../utils/constants'
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from('bookings')
     .select(
@@ -15,6 +16,13 @@ export async function getBookings({ filter, sortBy }) {
   // SORT
   if (sortBy)
     query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' })
+
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE
+    const to = from + PAGE_SIZE - 1
+
+    query = query.range(from, to)
+  }
 
   const { data: bookings, error, count } = await query
 
